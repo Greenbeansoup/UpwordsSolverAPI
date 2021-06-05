45ebc236bd4c97082af28c59482997d540d46eb9
Jacob Victor <successparrots@gmail.com>
2021-06-04 20:03:39 -0500

    First commit

---
+++ src/test/java/com/upwordsapi/upwords/UpwordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upwordsapi.upwords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpwordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
